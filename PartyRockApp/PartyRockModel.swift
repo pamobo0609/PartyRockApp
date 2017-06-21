f3cd2a16d4b41a4741edb9d0a9a2adcc2ce31bd4
pamobo0609 <jpmonge0609@gmail.com>
2017-06-21 15:46:21 -0600

    first commit

---
+++ PartyRockApp/PartyRockModel.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRockModel.swift
+//  PartyRockApp
+//
+//  Created by Liquid Workforce on 6/21/17.
+//  Copyright © 2017 Liquid Workforce. All rights reserved.
+//
+
+import Foundation
---
+++ PartyRockApp/PartyViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyViewCell.swift
+//  PartyRockApp
+//
+//  Created by Liquid Workforce on 6/21/17.
+//  Copyright © 2017 Liquid Workforce. All rights reserved.
+//
+
+import UIKit
+
+class PartyViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
